4b968a0ac12534a897bbe5db5ca16ba61abdc9ea
Duval Kilpatrick <mkilpatrick@yext.com>
2022-05-18 15:29:25 -0400

    Add documentation via comments in the template files

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,3 +1,13 @@
+/**
+ * This is an example of how to create a template that makes use of streams data.
+ * The stream data originates from Yext's Knowledge Graph. When a template in
+ * concert with a stream is built by the Yext Sites system, a static html page
+ * is generated for every corresponding (based on the filter) stream document.
+ * 
+ * Another way to think about it is that a page will be generated using this
+ * template for every eligible entity in your Knowledge Graph.
+ */
+
 import React from 'react';
 import Banner from '../components/banner';
 import Header from '../components/header';
@@ -11,14 +21,21 @@ import { reactWrapper } from '../wrapper';
 import { renderToString } from 'react-dom/server';
 import '../index.css';
 
+/**
+ * Required when Knowledge Graph data is used for a template.
+ */
 export const config = {
+  // The name of the feature. 
+  // NOTE: A future change may remove this and the feature name would use the name of the template by default.
   name: 'index',
-  hydrate: true,
-  streamId: 'products',
   stream: {
     $id: 'products',
+    // Required for now, but the plugin could set this automatically for you.
     source: 'knowledgeGraph',
+    // Required for now, but the plugin could set this automatically for you.
     destination: 'pages',
+    // Specifies the exact data that each generated document will contain. This data is passed in
+    // directly as props to the default exported function.
     fields: [
       'id',
       'uid',
@@ -37,9 +54,11 @@ export const config = {
       'c_relatedFAQs.question',
       'c_relatedFAQs.answer',
     ],
+    // Defines the scope of entities that qualify for this stream.
     filter: {
       entityTypes: ['location'],
     },
+    // The entity language profiles that documents will be generated for.
     localization: {
       locales: ['en'],
       primary: false,
@@ -47,10 +66,25 @@ export const config = {
   },
 };
 
+/**
+ * Defines the path that the generated file will live at for production.
+ * 
+ * NOTE: This currently has no impact on the local dev path. Local dev urls currently
+ * take on the form: featureName/entityId
+ */
 export const getPath = (data: any) => {
   return `index/${data.document.streamOutput.uid.toString()}`;
 };
 
+/**
+ * This is the main template. It can have any name as long as it's the default export.
+ * The props passed in here are the direct stream document defined by `config`.
+ * 
+ * There are a bunch of custom components being used from the src/components folder. These are
+ * an example of how you could create your own. You can set up your folder structure for custom
+ * components any way you'd like as long as it lives in the src folder (though you should not put
+ * them in the src/templates folder as this is specific for true template files).
+ */
 const Index = (props: any) => {
   const { document } = props;
   const { streamOutput } = document;
@@ -89,7 +123,16 @@ const Index = (props: any) => {
   );
 };
 
+
+/**
+ * Defines how the plugin will render the template for the production build. This has no
+ * impact on local dev.
+ * 
+ * A convenient function is currently defined in src/wrapper.ts.
+ * 
+ * NOTE: Future changes will probably remove the need for this function and wrapper.ts.
+ */
 export const render = (data: any) =>
-  reactWrapper(data, 'index', 'index.tsx', renderToString(<Index data={data} />), config.hydrate);
+  reactWrapper(data, 'index', 'index.tsx', renderToString(<Index data={data} />), true);
 
 export default Index;
