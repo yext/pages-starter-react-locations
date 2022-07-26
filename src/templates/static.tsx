cde504b4d2ae4d554d4b67255d46d47d32f4b205
lambdaFun <61638671+lambdaFun94@users.noreply.github.com>
2022-07-26 12:27:10 -0400

    add in tacos stuff (#19)
    * add in tacos stuff
    
    * updates based on duvals feedback
    
    * remove open until
    
    Co-authored-by: Andrew Sanchez <asanchez@yext.com>

--- src/templates/static.tsx
+++ src/templates/static.tsx
@@ -1,11 +1,9 @@
 /**
  * This is an example of how to create a static template that uses getStaticProps to retrieve data.
  */
-
 import * as React from "react";
-import { useState } from "react";
 import fetch from "fetch-everywhere";
-import { Pokemon } from "pokenode-ts";
+import "../index.css";
 import {
   Template,
   GetPath,
@@ -14,6 +12,9 @@ import {
   TemplateProps,
   TemplateRenderProps,
 } from "@yext/pages";
+import PageLayout from "../components/page-layout";
+import Card from "../components/card";
+import { ExternalImage } from "../types/ExternalImage";
 
 /**
  * Not required depending on your use case.
@@ -21,31 +22,34 @@ import {
 export const config: TemplateConfig = {
   // The name of the feature. If not set the name of this file will be used (without extension).
   // Use this when you need to override the feature name.
-  name: "pokemon",
+  name: "turtlehead-tacos",
 };
 
 /**
  * A local type for transformProps. This could live in src/types but it's generally
  * best practice to keep unshared types local to their usage.
  */
- type PokemonData = TemplateProps & { pokemon: Pokemon };
+type ExternalImageData = TemplateProps & { externalImage: ExternalImage };
 
- /**
-  * Used to either alter or augement the props passed into the template at render time.
-  * This function will be run during generation and pass in directly as props to the default
-  * exported function.
-  *
-  * This can be used when data needs to be retrieved from an external (non-Knowledge Graph)
-  * source. This example calls a public API and returns the data.
-  *
-  * If the page is truly static this function is not necessary.
-  */
- export const transformProps: TransformProps<PokemonData> = async (data) => {
-   const url = import.meta.env.YEXT_PUBLIC_POKEMON_API_BASE_URL;
-   const pokemon = (await fetch(url).then((res: any) => res.json())) as Pokemon;
-
-   return { ...data, pokemon };
- };
+/**
+ * Used to either alter or augment the props passed into the template at render time.
+ * This function will be run during generation and pass in directly as props to the default
+ * exported function.
+ *
+ * This can be used when data needs to be retrieved from an external (non-Knowledge Graph)
+ * source. This example calls a public API and returns the data.
+ *
+ * If the page is truly static this function is not necessary.
+ */
+export const transformProps: TransformProps<ExternalImageData> = async (
+  data
+) => {
+  const url = import.meta.env.YEXT_PUBLIC_EXTERNAL_IMAGE_API_BASE_URL + "/2";
+  const externalImage = (await fetch(url).then((res: any) =>
+    res.json()
+  )) as ExternalImage;
+  return { ...data, externalImage };
+};
 
 /**
  * Defines the path that the generated file will live at for production.
@@ -53,26 +57,65 @@ export const config: TemplateConfig = {
  * NOTE: This currently has no impact on the local dev path. Local dev urls currently
  * take on the form: featureName/entityId
  */
-export const getPath: GetPath<PokemonData> = () => {
+export const getPath: GetPath<ExternalImageData> = () => {
   return `static/${Math.random().toString()}`;
 };
 
-type PokemonRenderData = TemplateRenderProps & { pokemon: Pokemon };
+type ExternalImageRenderData = TemplateRenderProps & {
+  externalImage: ExternalImage;
+};
 
 /**
  * This is the main template. It can have any name as long as it's the default export.
  * The props passed in here are the direct result from `getStaticProps`.
  */
-const Static: Template<PokemonRenderData> = ({relativePrefixToRoot, path, document, pokemon}) => {
-  const { name } = pokemon;
-
-  const [num, setNum] = useState<number>(0);
+const Static: Template<ExternalImageRenderData> = ({
+  relativePrefixToRoot,
+  path,
+  document,
+  externalImage,
+}) => {
+  const { _site } = document;
 
   return (
     <>
-      <div>Hello from {name}</div>
-      <button onClick={() => setNum(num + 1)}>Click me</button>
-      Num: {num}
+      <PageLayout _site={_site}>
+        <div className="centered-container">
+          <div className="bg-red-900 text-5xl font-bold text-white p-10 flex items-center justify-center flex-col gap-x-14 gap-y-10 md:flex-row">
+            <h1>Welcome to Turtlehead Tacos</h1>
+          </div>
+          <div className="space-y-5">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+              velit, sed quia non numquam eius modi tempora incidunt ut labore
+              et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
+              veniam, quis nostrum exercitationem ullam corporis suscipit
+              laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
+              vel eum iure reprehenderit qui in ea voluptate velit esse quam
+              nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+              voluptas nulla pariatur?
+            </p>
+            <div>
+              <Card {...externalImage} />
+            </div>
+          </div>
+        </div>
+      </PageLayout>
     </>
   );
 };
