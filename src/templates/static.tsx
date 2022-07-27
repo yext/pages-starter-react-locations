ab9560ea881ebdc60a0ef97f2b7a661c7d630ca1
lambdaFun <61638671+lambdaFun94@users.noreply.github.com>
2022-07-27 14:09:30 -0400

    Avs port to tacos (#21)
    * add in tacos stuff
    
    * updates based on duvals feedback
    
    * remove open until
    
    * move to index.html
    
    * reformat index
    
    Co-authored-by: Andrew Sanchez <asanchez@yext.com>

--- src/templates/static.tsx
+++ src/templates/static.tsx
@@ -58,7 +58,7 @@ export const transformProps: TransformProps<ExternalImageData> = async (
  * take on the form: featureName/entityId
  */
 export const getPath: GetPath<ExternalImageData> = () => {
-  return `static/${Math.random().toString()}`;
+  return `index.html`;
 };
 
 type ExternalImageRenderData = TemplateRenderProps & {
