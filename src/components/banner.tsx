dd922ab3d7291686d7b9f8a63e4d86d709e8eb68
Aaron Pavlick <apavlick@yext.com>
2022-10-31 15:29:59 -0400

    styling fix

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -35,7 +35,7 @@ const Banner = (props: Banner) => {
   return (
     <>
       <div
-        className={`relative z-10w-full bg-cover bg-center h-96 bg-[url(/src/assets/images/tacos-1.avif)] `}
+        className={`relative z-10 w-full bg-cover bg-center h-96 bg-[url(/src/assets/images/tacos-1.avif)] `}
       >
         <div className="absolute left-0 right-0 flex flex-col items-center ">
           <div className="w-96 my-8 rounded-xl bg-amber-500 border-8 shadow-xl border-amber-600 px-4 py-2 text-center">
