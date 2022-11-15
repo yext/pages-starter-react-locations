4b038240c3820a245007150839794c4c528448b3
lymarrie <lucmarrie1@gmail.com>
2022-11-15 11:45:35 -0500

    updated getHeadConfig

--- src/templates/static.tsx
+++ src/templates/static.tsx
@@ -71,7 +71,7 @@ type ExternalImageRenderData = TemplateRenderProps & {
 
 /**
  * This allows the user to define a function which will take in their template
- * data and procude a HeadConfig object. When the site is generated, the HeadConfig
+ * data and produce a HeadConfig object. When the site is generated, the HeadConfig
  * will be used to generate the inner contents of the HTML document's <head> tag.
  * This can include the title, meta tags, script tags, etc.
  */
