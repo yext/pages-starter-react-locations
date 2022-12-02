fb3021645514f8507d7d8d8c373afe57258994eb
Tristan Timblin <ttimblin@yext.com>
2022-12-02 14:38:22 -0500

    docs: remove temporary note

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -63,9 +63,6 @@ export const config: TemplateConfig = {
 
 /**
  * Defines the path that the generated file will live at for production.
- *
- * NOTE: This currently has no impact on the local dev path. Local dev urls currently
- * take on the form: featureName/entityId
  */
 export const getPath: GetPath<TemplateProps> = ({ document }) => {
   return document.slug
