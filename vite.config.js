f139c685b8c662944e5271a260fb8e7819809498
Tristan Timblin <ttimblin@yext.com>
2022-12-21 15:50:41 -0500

    add .env

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import yextSSG from "@yext/pages/vite-plugin";
 
-export default defineConfig((context) => {
-  console.log(context, process.env);
-  return {
-    plugins: [react(), yextSSG()],
-  }
+export default defineConfig({
+  plugins: [react(), yextSSG()],
 });
