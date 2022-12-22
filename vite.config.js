35698db90268a843c588501ec46911bf0d7f5cfb
Tristan Timblin <ttimblin@yext.com>
2022-12-22 12:16:23 -0500

    exit code

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import yextSSG from "@yext/pages/vite-plugin";
 
-export default defineConfig({
-  plugins: [react(), yextSSG()],
+export default defineConfig(() => {
+  console.log("defineConfig()");
+  process.on('unhandledRejection', (reason, promise) => {
+    console.log("tris")
+    console.log(reason)
+    console.log("tan")
+    process.exit(1);
+  })
+  process.on('uncaughtException', (reason) => {
+    console.log("timb")
+    console.log(reason)
+    console.log("lin")
+  })
+  return {
+    plugins: [react(), yextSSG()],
+  }
 });
