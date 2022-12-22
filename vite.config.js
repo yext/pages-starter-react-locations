78c0df454580bec155ea74d9191f6d18c0fcad96
Tristan Timblin <ttimblin@yext.com>
2022-12-22 13:20:09 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import yextSSG from "@yext/pages/vite-plugin";
 
-export default defineConfig(() => {
-  console.log("defineConfig()");
+export default () => {
   process.on('unhandledRejection', (reason, promise) => {
+    console.log("outside");
     console.log(process.env);
     process.exit(1);
   });
-  process.on('uncaughtException', (reason) => {
-    console.log("timb")
-    console.log(reason)
-    console.log("lin")
-  })
-  return {
-    plugins: [react(), yextSSG()],
-  }
+
+  return defineConfig(() => {
+    console.log("defineConfig()");
+    process.on('unhandledRejection', (reason, promise) => {
+      console.log("inside");
+      console.log(process.env);
+      process.exit(1);
+    });
+    
+    process.on('uncaughtException', (reason) => {
+      console.log("timb")
+      console.log(reason)
+      console.log("lin")
+    })
+    return {
+      plugins: [react(), yextSSG()],
+    }
+  });
 });
