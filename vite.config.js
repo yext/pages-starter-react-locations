d1abef84190c5e2546d1cc7370028c2ed87314fe
Tristan Timblin <ttimblin@yext.com>
2023-01-16 10:46:02 -0500

    remove plugin

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,5 @@ import yextSSG from "@yext/pages/vite-plugin";
 export default defineConfig({
   base: "/subdirectory",
   appType: 'mpa',
-  plugins: [react(), yextSSG(), {
-    name: 'rewrite-middleware',
-    configureServer(serve) {
-      serve.middlewares.use((req, res, next) => {
-        if (req.url.startsWith('/subdirectory/')) {
-          req.url = '/subdirectory/'
-        }
-        next()
-      })
-    }
-  }],
+  plugins: [react(), yextSSG()],
 });
