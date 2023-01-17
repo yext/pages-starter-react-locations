02ced3848489641874dbf7d0a5ecce852460df56
Tristan Timblin <ttimblin@yext.com>
2023-01-17 13:10:35 -0500

    import image

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Cta from "./cta";
+import taco from "../assets/images/taco.jpeg";
 
 export type Address = {
   line1: string;
@@ -31,8 +32,9 @@ const Banner = (props: Banner) => {
 
   return (
     <>
+    <img src={taco} alt="taco" />
       <div
-        className={`relative z-10 w-full bg-cover bg-center h-96 bg-[url(/assets/images/tacos-1.avif)] `}
+        className={`relative z-10 w-full bg-cover bg-center h-96 bg-[url(src/assets/images/tacos-1.avif)] `}
       >
         <div className="absolute left-0 right-0 flex flex-col items-center ">
           <div className="w-96 my-8 rounded-xl bg-amber-500 border-8 shadow-xl border-amber-600 px-4 py-2 text-center">
