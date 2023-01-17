e5078fe444de5c21d9b81ed12a6814917ad13bf6
Tristan Timblin <ttimblin@yext.com>
2023-01-17 11:54:22 -0500

    blank

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import yextSSG from "@yext/pages/vite-plugin";
 
 export default defineConfig({
-  base: "../",
+  base: "",
   plugins: [react(), yextSSG()],
   build: {
     build: {
