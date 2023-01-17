47d1a7697baadf23399ea2b8def20ce233fe3e65
Tristan Timblin <ttimblin@yext.com>
2023-01-17 13:58:09 -0500

    remove build config

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,4 @@ import yextSSG from "@yext/pages/vite-plugin";
 export default defineConfig({
   base: "/subdirectory/",
   plugins: [react(), yextSSG()],
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img';
-          }
-          return `subdirectory/assets/${extType}/[name]-[hash][extname]`;
-        },
-        chunkFileNames: 'subdirectory/assets/js/[name]-[hash].js',
-        entryFileNames: 'subdirectory/assets/js/[name]-[hash].js',
-      },
-    },
-  },
 });
