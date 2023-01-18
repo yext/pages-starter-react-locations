b897b9b379b5aa6579867f43be88babc64257f23
Tristan Timblin <ttimblin@yext.com>
2023-01-18 13:51:51 -0500

    bump

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import react from "@vitejs/plugin-react";
 import yextSSG from "@yext/pages/vite-plugin";
 
 export default defineConfig({
-  base: "./",
+  base: "../",
   plugins: [react(), yextSSG()],
-  transform: (code, id) => {
-    return {
-      code: code.replace(/\/src\/(.*)\.(svg|jp?g|png|webp)/, 'http://localhost:3000/src/$1.$2'),
-      map: null,
-    }
-  },
 });
