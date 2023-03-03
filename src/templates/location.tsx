8ab72579cadb88af2b237b067d9cfb1ced57bd6b
lymarrie <lucmarrie1@gmail.com>
2023-03-03 00:54:22 -0500

    updated location stream

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -57,7 +57,10 @@ export const config: TemplateConfig = {
       "geocodedCoordinate",
       "services",
       "photoGallery",
-      "dm_directoryParents"
+      "dm_directoryParents.name",
+      "dm_directoryParents.slug",
+      "dm_directoryParents.meta",
+      "dm_directoryParents.c_addressRegionDisplayName",    
     ],
     // The entity language profiles that documents will be generated for.
     localization: {
