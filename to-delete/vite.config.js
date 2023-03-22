566e0c51f3972fddba4f565e6b9db2694eb301ab
Andrew Sanchez <asanchez@yext.com>
2023-03-22 17:55:47 -0400

    remove unnecessary directory

--- to-delete/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import yextSSG from "@yext/pages/vite-plugin";
-
-export default defineConfig({
-  plugins: [react(), yextSSG()],
-});
