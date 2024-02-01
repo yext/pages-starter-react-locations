22c0193513e51d1eb6065d1b1b6cc23849b88562
lymarrie <lucmarrie1@gmail.com>
2024-02-01 10:15:25 -0500

    updated static.tsx

--- src/templates/static.tsx
+++ src/templates/static.tsx
@@ -48,7 +48,7 @@ type ExternalImageData = TemplateProps & { externalImage: ExternalImage };
 export const transformProps: TransformProps<ExternalImageData> = async (
   data
 ) => {
-  const url = YEXT_PUBLIC_EXTERNAL_IMAGE_API_BASE_URL + "/2";
+  const url = `https://jsonplaceholder.typicode.com/photos/2`;
   const externalImage = (await fetch(url).then((res: any) =>
     res.json()
   )) as ExternalImage;
